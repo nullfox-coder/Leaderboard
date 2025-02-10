92d7ad93c08e646673f86f7f8e94aadcf2424605
null_coder <104222189+nullfox-coder@users.noreply.github.com>
2025-02-10 14:19:53 +0530

    env change

--- leaderboard/leaderboard/env.py
+++ leaderboard/leaderboard/env.py
@@ -3,24 +3,24 @@ import os
 
 
 def get_credentials():
-    creds = {}
-    if "env" in os.environ:
-        creds['debug'] = os.environ['debug']
-        creds['allowed_hosts'] = os.environ['allowed_hosts']
-        creds['django_secret_key'] = os.environ['django_secret_key']
-        creds['db_password'] = os.environ['db_password']
-        creds['db_name'] = os.environ['db_name']
-        creds['db_user'] = os.environ['db_user']
-        creds['db_host'] = os.environ['db_host']
-        creds['cors_origin_whitelist'] = os.environ['cors_origin_whitelist']
-        creds['aws_access_key'] = os.environ['aws_access_key']
-        creds['aws_secret_key'] = os.environ['aws_secret_key']
-        creds['s3_bucket'] = os.environ['s3_bucket']
-    else:
+        creds = {}
+    # if "env" in os.environ:
+    #     creds['debug'] = os.environ['debug']
+    #     creds['allowed_hosts'] = os.environ['allowed_hosts']
+    #     creds['django_secret_key'] = os.environ['django_secret_key']
+    #     creds['db_password'] = os.environ['db_password']
+    #     creds['db_name'] = os.environ['db_name']
+    #     creds['db_user'] = os.environ['db_user']
+    #     creds['db_host'] = os.environ['db_host']
+    #     creds['cors_origin_whitelist'] = os.environ['cors_origin_whitelist']
+    #     creds['aws_access_key'] = os.environ['aws_access_key']
+    #     creds['aws_secret_key'] = os.environ['aws_secret_key']
+    #     creds['s3_bucket'] = os.environ['s3_bucket']
+    # else:
         env_file_dir = os.path.dirname(
             os.path.dirname(os.path.abspath(__file__)))
         with open(os.path.join(env_file_dir, '.env.json'), 'r') as f:
             creds = json.loads(f.read())
-    return creds
+        return creds
 
 credentials = get_credentials()
